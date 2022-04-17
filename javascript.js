2ae13bc36a9e5a6eca7ebfa41be0f18f158cacb7
BogunPathmaraj <bpathmar@my.centennialcollege.ca>
2022-04-17 14:45:19 -0400

    Dynamic Grid Complete

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ let container = document.getElementById('container')
 
  
 
-let gridSize = 4;
+let gridSize = prompt("Enter the grid size");
 
 createGrid(gridSize);
 
@@ -10,18 +10,34 @@ createGrid(gridSize);
  {
     for (let rows = 0; rows < gridSize; rows++)
     {
+        
+
         for (let columns = 0; columns < gridSize; columns++)
        {
-     
+        
          let tag = document.createElement("div");
-         tag.innerHTML = "<div class='gridbox'> </div>"
+         tag.innerHTML = `<div class='gridbox'> ${columns} </div>`
        container.append( tag)
+      
+      
+      
+       let boxes = document.getElementsByClassName('gridbox')
+
+       for (let i =0; i < boxes.length; i++)
+       {
+        let dimension = ((960 - gridSize*2)/(gridSize) + 'px').toString(); 
+              boxes[i].style.height = `${dimension}` 
+              boxes[i].style.width = `${dimension}`
+              console.log(boxes[i] )
+              console.log(dimension)
+       }
        }
             
     }
      
+     
+
  }
    
-function dynamicContainer() {
 
-}
+ 
