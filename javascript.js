0571e236523c1ca5bc5a6ac2c5a94a020ef86dc4
BogunPathmaraj <bpathmar@my.centennialcollege.ca>
2022-04-19 00:14:56 -0400

    Reset Button Created

--- javascript.js
+++ javascript.js
@@ -1,11 +1,17 @@
 let container = document.getElementById('container')
 
- 
-
 let gridSize = prompt("Enter the grid size");
 
-createGrid(gridSize);
+let boxes = document.getElementsByClassName('gridbox')
 
+  createGrid(gridSize);
+
+  
+let button = document.getElementById('reset-btn') 
+   button.addEventListener('click',() => resetGrid(boxes))
+
+
+//The Dynamic Grid creation 
  function createGrid(gridSize)
  {
     for (let rows = 0; rows < gridSize; rows++)
@@ -16,28 +22,38 @@ createGrid(gridSize);
        {
         
          let tag = document.createElement("div");
-         tag.innerHTML = `<div class='gridbox'> ${columns} </div>`
-       container.append( tag)
+         tag.innerHTML = `<div class='gridbox'>   </div>`
+          container.append( tag)
       
-      
-      
-       let boxes = document.getElementsByClassName('gridbox')
-
-       for (let i =0; i < boxes.length; i++)
-       {
-        let dimension = ((960 - gridSize*2)/(gridSize) + 'px').toString(); 
-              boxes[i].style.height = `${dimension}` 
-              boxes[i].style.width = `${dimension}`
-              console.log(boxes[i] )
-              console.log(dimension)
-       }
-       }
+          for (let i =0; i < boxes.length; i++)
+            {
+            let dimension = ((960 - gridSize*2)/(gridSize) + 'px').toString(); 
+                  boxes[i].style.height = `${dimension}` 
+                  boxes[i].style.width = `${dimension}`
+                  boxes[i].addEventListener('mouseover', () => {boxes[i].style.backgroundColor = 'red'})
+            }
             
-    }
-     
-     
+            
+                 
+      }
+            
+    }      
 
- }
+ } 
+
+ 
    
+//The Random Color Generator
+function randomColor(){
 
+}
+
+//Reset Grid color
+function resetGrid(boxes )
+{
+   for (let i =0; i < boxes.length; i++)
+      {
+         boxes[i].style.backgroundColor = 'antiquewhite'
+      }
+}
  
